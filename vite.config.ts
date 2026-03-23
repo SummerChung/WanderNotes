0d33fbb7b8c87bf9ea7f8d999d5e569069899d6b
SummerChung <summerchung20@gmail.com>
2026-03-23 13:35:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/WanderNotes/', // 👈 這裡一定要有，且名稱要跟你的 GitHub 儲存庫名稱完全一樣
+  base: '/WanderNotes/', // 👈 這裡的名稱必須跟你的 GitHub 儲存庫名稱「完全一模一樣」
 });
